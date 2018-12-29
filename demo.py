5e7f16fb4eb73423c09cb90e74d2c976ce6b75dc
Prevalence <945926945@qq.com>
2018-12-29 11:15:19 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -8,11 +8,10 @@ def get_available_gpus():
     return [x.name for x in local_device_protos if x.device_type == 'GPU']
 
 if __name__ == "__main__":
-    import tensorflow as tf
 
     a = tf.constant([1.0, 2.0, 3.0], shape=[3], name='a')
     b = tf.constant([1.0, 2.0, 3.0], shape=[3], name='b')
     c = a + b
     # 通过log_device_placement参数来输出运行每一个运算的设备。
     sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-    print sess.run(c)
+    print(sess.run(c))
